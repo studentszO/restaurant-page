3a5d28d7fc51a23598d93c179d4e0cea4a345a98
szOstudent <169681673+studentszO@users.noreply.github.com>
2024-09-05 17:39:31 +0200

    Added an event that, when a button is clicked, the content of the page changes

--- src/script.js
+++ src/script.js
@@ -1,5 +1,33 @@
 import "./style.css";
-import "./homepage.js";
+import { homepage } from "./homepage.js";
+import { ourStory } from "./ourstory.js";
 
-const nav = document.querySelector("nav");
-console.log(nav)
+const buttons = document.querySelectorAll("button")
+homepage();
+
+let setPageId = 0;
+const contentContainer = document.querySelector("#content");
+
+buttons.forEach((e) => e.addEventListener("click", () => {
+    const id = Array.from(e.parentNode.children).indexOf(e);
+    contentContainer.textContent = "";
+    console.log(setPageId )
+
+    if (id === 1) {
+        ourStory();
+        setPageId = 1
+    }
+    else if (id === 0 && setPageId !== 0) {
+        homepage();
+        setPageId = 0;
+    }
+    // else if (id === 2) {
+    //     setPageId = 2;
+    // }
+    // else if (id === 3) {
+    //     setPageId = 3;
+    // }
+    // else if (id === 4) {
+    //     setPageId = 4;
+    // }
+}));
