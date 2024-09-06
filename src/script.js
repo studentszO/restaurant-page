c37fa3a2898e7bdaefad834d91e4d49941e05463
szOstudent <169681673+studentszO@users.noreply.github.com>
2024-09-06 11:47:45 +0200

    Added styling to the navbar & fixed a bug when the content of the page was removed if you clicked on the home button & if you were already in the home page

--- src/script.js
+++ src/script.js
@@ -12,22 +12,27 @@ const contentContainer = document.querySelector("#content");
 
 buttons.forEach((e) => e.addEventListener("click", () => {
     const id = Array.from(e.parentNode.children).indexOf(e);
-    contentContainer.textContent = "";
-    console.log(setPageId )
+    // e.style.backgroundColor = "var(--main-clr)"
+    // contentContainer.textContent = "";
+    console.log(setPageId)
 
     if (id === 0 && setPageId !== 0) {
+        contentContainer.textContent = "";
         homepage();
         setPageId = 0;
     }
     if (id === 1) {
+        contentContainer.textContent = "";
         ourStory();
         setPageId = 1
     }
     else if (id === 2) {
+        contentContainer.textContent = "";
         setPageId = 2;
         theMenu();
     }
     else if (id === 3) {
+        contentContainer.textContent = "";
         hoursAndLocation();
         setPageId = 3;
     }
